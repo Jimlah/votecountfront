c8462f3f0ead61bb2803a39e2dca9fba240193ac
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-31 08:20:28 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        Hey
+      </nav>
+      <main>
+        Hoy you doing
+      </main>
+    </div>
+  );
+}
+
+export default App;
