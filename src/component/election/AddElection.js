20b763b5d994f84bbbab3202e5fbb112ab55984b
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-31 11:48:39 +0100

    Update AddElection.js

--- src/component/election/AddElection.js
+++ src/component/election/AddElection.js
@@ -1,3 +1,4 @@
+import electionService from "../../services/election.service";
 import useInput from "./../input/useInput";
 const AddElection = () => {
   const styleClass =
@@ -7,26 +8,46 @@ const AddElection = () => {
   const [description, bindDescription, resetDescription] = useInput("");
   const [electionDate, bindElectionDate, resetElectionDate] = useInput("");
 
-  const handleSubmit = () => {
-      
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let data = {
+      title: title,
+      description: description,
+      election_date: electionDate,
+    };
+
+    electionService
+      .create(data)
+      .then((response) => {
+        resetTitle();
+        resetDescription();
+        resetElectionDate();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="">
       <div>{"working"}</div>
-      <input type="text" name="title" {...bindTitle} className={styleClass} />
-      <input
-        type="text"
-        name="description"
-        {...bindDescription}
-        className={styleClass}
-      />
-      <input
-        type="text"
-        name="election_date"
-        {...bindElectionDate}
-        className={styleClass}
-      />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" {...bindTitle} className={styleClass} />
+        <input
+          type="text"
+          name="description"
+          {...bindDescription}
+          className={styleClass}
+        />
+        <input
+          type="text"
+          name="election_date"
+          {...bindElectionDate}
+          className={styleClass}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
